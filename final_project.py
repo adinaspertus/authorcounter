5e823aed484b05d6e3cc59b61757343b33a43b7d
adina spertus <adina22@gmail.com>
2020-10-18 15:37:06 +0200

    update import to pandas
    create a new script called "achrive_script.py" that includes old code we don't want to delete. currently includes original method for importing abstracts as a list of dictionaries rather than into pandas

--- final_project.py
+++ final_project.py
@@ -10,45 +10,39 @@ Created on Sun Oct 18 10:14:51 2020
 
 
 import json
+import pandas as pd
 
 
+#pre-processing of the data
 f = open("data/arxiv-metadata-oai-snapshot.json")
 
+data  = []
 counter = 0
-papers = []
-for item in f:
-    if counter < 100:
-        item = json.loads(item)
-#        print(item["authors"]) # comment out when you don't wnat a list of authors
-        entry = {"doi":item["doi"], 
-                 "title":item["title"],
-                 "authors":item["authors"],
-                 "year":item["journal-ref"], # need to pull last four digits 
-                 "abstract":item["abstract"],
-                 }
-        papers.append(entry) #papers is a list of dictionaries
-        counter += 1
-    else:
-        break
-        
+with open("data/arxiv-metadata-oai-snapshot.json", "r") as f:
+    for line in f: 
+        if counter < 100:
+            data.append(json.loads(line))
+            counter += 1
 f.close()
 
-# develop a program in a different file for counting authors
-# ignore anything in a parentheses
-# find all ", and" and replace with an *
-# spit is either ", " "and" or "*"        
-        
+
+dict_ = {"doi": [], "title": [], "authors": [], "date": [], "abstract": []}
+for paper in data:
+    dict_["doi"].append(paper["doi"]),
+    print(dict_["doi"]),
+    dict_["title"].append(paper["title"]),
+    print(dict_["title"])
+    dict_["authors"].append(paper["authors"]),
+    dict_["date"].append(paper["update_date"]),
+    dict_["abstract"].append(paper["abstract"])
+
+df = pd.DataFrame(dict_, columns=["doi", "title", "authors", "date", "abstract"])
+df.head(5)
+df.info()
 
 
-# change no. to call different papers
 
-papers[1]["abstract"] #we'll need to clean out the \n
-papers[1]["title"]
-papers[1]["authors"]
-#papers[1]["year"]
 
-#see whole entry
-papers[2]
 
 
 for i in papers:
