0bcd3ad8ed0539abc1383bba02134b1a66c60fee
adina spertus <adina22@gmail.com>
2020-10-18 13:51:10 +0200

    comments on next steps
    to do: pull out year and develop separate program for counting number of authors

--- final_project.py
+++ final_project.py
@@ -5,11 +5,12 @@ Created on Sun Oct 18 10:14:51 2020
 
 @author: adina
 """
+#https://www.kaggle.com/Cornell-University/arxiv
+#https://arxiv.org/help/api/user-manual#python_simple_example
 
 
 import json
-#import os
-#print(os.getcwd())
+
 
 f = open("data/arxiv-metadata-oai-snapshot.json")
 
@@ -18,20 +19,27 @@ papers = []
 for item in f:
     if counter < 100:
         item = json.loads(item)
+#        print(item["authors"]) # comment out when you don't wnat a list of authors
         entry = {"doi":item["doi"], 
                  "title":item["title"],
                  "authors":item["authors"],
-                 #"year":item[...], # need to pull this from ___
+                 "year":item["journal-ref"], # need to pull last four digits 
                  "abstract":item["abstract"],
                  }
-        papers.append(entry)
+        papers.append(entry) #papers is a list of dictionaries
         counter += 1
     else:
         break
         
 f.close()
+
+# develop a program in a different file for counting authors
+# ignore anything in a parentheses
+# find all ", and" and replace with an *
+# spit is either ", " "and" or "*"        
         
-        
+
+
 # change no. to call different papers
 
 papers[1]["abstract"] #we'll need to clean out the \n
@@ -43,3 +51,7 @@ papers[1]["authors"]
 papers[2]
 
 
+for i in papers:
+    print(papers[i]["authors"])
+
+#print(papers[]["authors"])
