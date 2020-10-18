6a74b33a29dd3fcb1a2607ad63597d565ec079a2
adina spertus <adina22@gmail.com>
2020-10-18 10:53:44 +0200

    initialize first stage
    includes first stage of program (import article metadata) and a gitignore file to prevent git commits from uploading data folder

---
+++ final_project.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 18 10:14:51 2020
+
+@author: adina
+"""
+
+#https://www.kaggle.com/maksimeren/arxiv-literature-clustering
+
+import json
+import os
+print(os.getcwd())
+
+f = open("data/arxiv-metadata-oai-snapshot.json")
+
+counter = 0
+papers = []
+for item in f:
+    if counter < 100:
+        item = json.loads(item)
+        entry = {"doi":item["doi"], 
+                 "title":item["title"],
+                 "authors":item["authors"],
+                 #"year":item[...],
+                 "abstract":item["abstract"],
+                 }
+        papers.append(entry)
+        counter += 1
+    else:
+        break
+        
+f.close()
+        
+        
+# change no. to call different papers
+
+print(papers[1]["abstract"]) #we'll need to clean out the \n
+papers[1]["title"]
+papers[1]["authors"]
+#papers[1]["year"]
+
+
+
+#see whole entry
+papers[2]
+
+
