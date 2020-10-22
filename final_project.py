5a1481553dcb9a7dd232e640e098361fc58dd7ed
adina spertus <adina22@gmail.com>
2020-10-22 15:25:52 +0200

    clean text
    added lines to clean text including regex. one line taken modified from: https://towardsdatascience.com/text-cleaning-methods-for-natural-language-processing-f2fc1796e8c7

--- final_project.py
+++ final_project.py
@@ -12,10 +12,11 @@ Created on Sun Oct 18 10:14:51 2020
 import json
 import pandas as pd
 #import name_counter as nc
-import re
+import re #remove if we get name_counter to different script
+import nltk
 
-# define name counter program (move to new script later?)
-# Comment any edits to the name_counter program
+# define name counter program (move to new script later)
+# NOTE: Please comment any edits to the name_counter program
 
 def name_counter(n):
     #import re
@@ -34,10 +35,12 @@ def name_counter(n):
     a = n.split("$$")
     return(len(a))
     
+#see if name_counter is working (useful testing imported version of it)
+print((name_counter("adina, ofer, hannah, and huey")))
 
 #pre-processing of the data
+#import subset of data from file
 f = open("data/arxiv-metadata-oai-snapshot.json")
-
 data  = []
 counter = 0
 with open("data/arxiv-metadata-oai-snapshot.json", "r") as f:
@@ -47,7 +50,7 @@ with open("data/arxiv-metadata-oai-snapshot.json", "r") as f:
             counter += 1
 f.close()
 
-
+#create dictionary to convert into panda
 dict_ = {"doi": [], "title": [], "authors": [], "a_count": [], "date": [], "abstract": []}
 for paper in data:
     dict_["doi"].append(paper["doi"]),
@@ -57,27 +60,35 @@ for paper in data:
     dict_["date"].append(paper["update_date"]),
     dict_["abstract"].append(paper["abstract"]),
     
-#column names
+#set panda column names
 df = pd.DataFrame(dict_, columns=["doi", "title", "authors", "a_count", "date", "abstract"])
 df.head(5)
 df.info()
 
-print((name_counter("adina, ofer, hannah, and huey")))
+# we could define a function that does this called "clean" or something 
+# and then import it in from a separate script
 
-#converting abstract columns into lists of lowercase words
+#convert abstract columns into lists of lowercase words
+df["abstract"] = df["abstract"].str.lower()
+#remove latex (anything within $...$)
+df["abstract"] = df["abstract"].apply(lambda elem: re.sub(r"\$.+\$", "", elem))
+#special characters (this line modified from: https://towardsdatascience.com/text-cleaning-methods-for-natural-language-processing-f2fc1796e8c7
+df["abstract"] = df["abstract"].apply(lambda elem: re.sub(r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", " ", elem))  
+#remove numbers
+df["abstract"] = df["abstract"].apply(lambda elem: re.sub(r"\d+", "", elem))
+#remove extra spaces (replace all space chunks with one space)
+df["abstract"] = df["abstract"].apply(lambda elem: re.sub(r"[\s]+", " ", elem))
 
-df["abstract"]= df["abstract"].str.lower()
-print(df["abstract"])
+print(df["abstract"][2])
+
+
 
 #split with a space
 #we need to clean the abstract texts before this 
 df["ab_split"] = list(df["abstract"].str.split(" "))
 print(df["ab_split"])
 
-               
-
-
-
+              
 
 # Create a copy of the DataFrame to work from
 # Omit random state to have different random split each run
