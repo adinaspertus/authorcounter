f6a636d31c0268f3b926fbb29c9a15e696b5aa97
Ofer <Oferikad@gmail.com>
2020-10-25 13:09:56 +0100

    Update authorcounter_app.py

--- authorcounter_app.py
+++ authorcounter_app.py
@@ -14,34 +14,40 @@ from tkinter import *
 root = Tk()
 #resizing the size of the app window
 root.geometry("1000x1000+150+150")
+
+
 #creating main label 
-label = Label(root, text="Author Counter")
-#pushing it onto the screen
-label.pack() #to manually set the label place use label.grid(row=, column=)
+label = Label(root, text="Author Counter", fg="aquamarine4")
+label.config(font=("Courier", 44))
+label.grid(row=0, column=2, sticky=E+W)#pushing it onto the screen
+#sub label
+sub_label = Label(root, text="Discover how many authors wrote the text you are reading with", fg="aquamarine4")
+sub_label.config(font=("Courier", 24))
+sub_label.grid(row=1, column=2, sticky=E+W)#sticky aims to keep in same location but need to learn it better 
 
-#main space within root
-main = Canvas(root) 
-main_panel = Canvas()
-main.pack()
-
-
+entry_label = Label(root, text = "Please insert text in the box below",fg="dark slate gray")
+entry_label.config(font=("Courier", 18))
+entry_label.grid(row=4, column=2, sticky=E+W)
 
 #define a function what happens when clicking the button
+#for colours check http://www.science.smith.edu/dftwiki/index.php/Color_Charts_for_TKinter
+
 #elements for the function
 #1 create entrybox with a sting variable content
 entry_text = StringVar() # the text in  your entry
-entry_widget = Entry(main, width = 20, textvariable = entry_text) # the entry
-main.create_window(50, 50, window = entry_widget)
+entry_widget = Entry(root,  width = 100, bg="wheat2", fg="white", textvariable = entry_text) # the entry
+entry_widget.grid(row=5, column=2, ipady=80, sticky=E+W)
+
+
 
 #create entrybox
 
 #create the button to attach to the entry box
 button = Button(root, text="Submit text") # to attach to entry box add command= function name
-button.pack()
+button.grid(row=6, column=2, sticky=E+W)
 
 
 #to attach button to function use command= function_name)
-
 
 
 
